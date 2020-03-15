5ab66aedb2fa9480957389073461cf841f9f095f
Sainikhil <sainikhilnaru@gmail.com>
2020-03-15 00:27:47 -0500

    lab 3

--- app/src/main/java/com/example/trivia_app/MainActivity.java
+++ app/src/main/java/com/example/trivia_app/MainActivity.java
@@ -1,13 +1,14 @@
 package com.example.trivia_app;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.EditText;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +16,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        flashcardDatabase = new FlashcardDatabase(getApplicationContext());
+        allFlashcards = flashcardDatabase.getAllCards();
+
+        if (allFlashcards != null && allFlashcards.size() > 0) {
+            ((TextView) findViewById(R.id.flashcard_question1)).setText(allFlashcards.get(0).getQuestion());
+            ((TextView) findViewById(R.id.flashcard_answer1)).setText(allFlashcards.get(0).getAnswer());
+        }
+
         findViewById(R.id.flashcard_question1).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -25,23 +35,66 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        findViewById(R.id.flashcard_answer1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Do something here
+                findViewById(R.id.flashcard_question1).setVisibility(View.VISIBLE);
+                findViewById(R.id.flashcard_answer1).setVisibility(View.INVISIBLE);
+
+            }
+        });
+
         findViewById(R.id.addNewCardButton).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, addCardActivity.class);
-                startActivity(intent);
+                MainActivity.this.startActivityForResult(intent,100);
             }
         });
+
+
+        findViewById(R.id.nextCardButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                currentCardDisplayedIndex++;
+
+                if (currentCardDisplayedIndex > allFlashcards.size() - 1) {
+                    currentCardDisplayedIndex = 0;
+                }
+
+                ((TextView) findViewById(R.id.flashcard_question1)).setText(allFlashcards.get(currentCardDisplayedIndex).getQuestion());
+                ((TextView) findViewById(R.id.flashcard_answer1)).setText(allFlashcards.get(currentCardDisplayedIndex).getAnswer());
+            }
+        });
+
+        findViewById(R.id.deleteCard).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                flashcardDatabase.deleteCard(((TextView) findViewById(R.id.flashcard_question1)).getText().toString());
+                allFlashcards = flashcardDatabase.getAllCards();
+
+            }
+        });
+
     }
+    FlashcardDatabase flashcardDatabase;
+    List<Flashcard> allFlashcards;
+
+    int currentCardDisplayedIndex = 0;
 
     @Override
     protected  void onActivityResult(int requestcode, int resultcode, Intent data){
-        if (requestcode == 100) { // this 100 needs to match the 100 we used when we called startActivityForResult!
+        if (requestcode == 100 ) { // this 100 needs to match the 100 we used when we called startActivityForResult!
             String string1 = data.getExtras().getString("newCardQuestion"); // 'string1' needs to match the key we used when we put the string in the Intent
             String string2 = data.getExtras().getString("newAnswerQuestion");
-            //Log.d(string1, "onActivityResult() returned: " );
+            Log.d(string1, "onActivityResult() returned: " );
             ((TextView) findViewById(R.id.flashcard_question1)).setText(string1);
             ((TextView) findViewById(R.id.flashcard_answer1)).setText(string2);
+            flashcardDatabase.insertCard(new Flashcard(string1, string2));
+            allFlashcards = flashcardDatabase.getAllCards();
+
         }
     }
+
 }
--- app/src/main/java/com/example/trivia_app/addCardActivity.java
+++ app/src/main/java/com/example/trivia_app/addCardActivity.java
@@ -1,12 +1,11 @@
 package com.example.trivia_app;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class addCardActivity extends AppCompatActivity {
 
@@ -14,6 +13,7 @@ public class addCardActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_card2);
+
         findViewById(R.id.cancelNewCardButton).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -25,15 +25,14 @@ public class addCardActivity extends AppCompatActivity {
         findViewById(R.id.saveButton).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(addCardActivity.this, MainActivity.class);
-                startActivityForResult(intent,100);
                 Intent data = new Intent();
                 String question = ((EditText) findViewById(R.id.editQuestion)).getText().toString();
                 String answer = ((EditText) findViewById(R.id.editAnswer)).getText().toString();
                 data.putExtra("newCardQuestion", question);
                 data.putExtra("newAnswerQuestion", answer);
-                //Log.d(question, "onActivityResult() returned: " );
-                setResult(101,data);
+                //Log.d(question, "save button clicked and question data outputted: " );
+                //above successively returned
+                setResult(RESULT_OK,data);
                 finish();
             }
         });
